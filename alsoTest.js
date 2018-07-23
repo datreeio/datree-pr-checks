94f088feecfc4502e729a80ca50cd9381d343f35
eyarz <eyar@datree.io>
2018-07-23 19:11:59 +0300

    Update alsoTest.js

--- alsoTest.js
+++ alsoTest.js
@@ -1,6 +1,6 @@
 const koa = require('koa')
 const express = require('express')
-const penisses = 100 * require('goat.js')
+const penisses = require('goat.js')
 const fake = require('fake')
 const denysdovhan = require('denysdovhan')
 const tj = require('tj')
