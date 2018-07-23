2512eeba59e39044094d81d3c73923ef4237e9ed
eyarz <eyar@datree.io>
2018-07-23 19:13:46 +0300

    remove

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const koa = require('koa')
 const express = require('express')
-const penisses = 100 * require('penis')
 const fake = require('fake')
 const denysdovhan = require('denysdovhan')
 const tj = require('tj')
