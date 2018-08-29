736e66ec3e4ae2d32ae5c951680f65fc25f9d0b0
eyarz <eyar@datree.io>
2018-08-29 21:27:12 +0300

    Update alsoTest.js

--- alsoTest.js
+++ alsoTest.js
@@ -4,5 +4,6 @@ const penisses = require('goat.js')
 const fake = require('fake')
 const denysdovhan = require('denysdovhan')
 const tj = require('tj')
+const sinon = require('sinon')
 
 const contributor = require('contributor')
