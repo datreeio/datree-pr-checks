b6bb9027a8b7b6d79e48e5d2c1e41575f55b5c68
John Doe <fake-email@something.com>
2018-08-29 21:51:08 +0300

    remove code

--- alsoTest.js
+++ alsoTest.js
@@ -1,6 +1,5 @@
 const koa = require('koa')
 const express = require('express')
-const penisses = require('goat.js')
 const fake = require('fake')
 const denysdovhan = require('denysdovhan')
 const tj = require('tj')
