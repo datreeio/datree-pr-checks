04e64858515074c726e9792709ace5f16a42b7ae
eyar <eyar@datree.io>
2018-08-29 21:52:51 +0300

    remove code

--- alsoTest.js
+++ alsoTest.js
@@ -2,7 +2,6 @@ const koa = require('koa')
 const express = require('express')
 const fake = require('fake')
 const denysdovhan = require('denysdovhan')
-const tj = require('tj')
 const sinon = require('sinon')
 
 const contributor = require('contributor')
